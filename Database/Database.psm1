1e08318e4ea697d5d71fcd10e3015ff369a3ddaa
Carlos Perez <carlos_perez@darkoperator.com>
2013-03-16 09:47:38 -0400

    Organize better the functions in to sub-modules

---
+++ Database/Database.psm1
@@ -0,0 +1,173 @@
+﻿$Global:sqliteconn = New-Object System.Collections.ArrayList
+
+function Connect-SQLite3 
+{
+    [CmdletBinding()]
+	param (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [ValidateScript({Test-Path $_})]
+        [string]$DataBase,
+        [switch]$ReadOnly,
+        [SecureString]$Password
+    )
+    
+    Begin
+    {
+        # Load the appropiate DLL Depending on the Archiecture
+	    switch ([intptr]::size)
+	    {
+		    4 {$sqlitedll = [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\x86\System.Data.SQLite.dll")} 
+		    8 {$sqlitedll = [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\x64\System.Data.SQLite.dll")}
+	    }
+    }
+    Process
+    {
+	    $cn = New-Object -TypeName System.Data.SQLite.SQLiteConnection
+	    $cn.ConnectionString = "Data Source=$DataBase"
+	    $cn.Open()
+        $conn_obj = $cn
+        if ($Global:sqliteconn -notcontains $conn_obj)
+        {
+            $SessionIndex = $Global:sqliteconn.Count
+            write "index is $SessionIndex"
+            $NewConnection = New-Object psobject -Property @{
+                                Index = $SessionIndex.ToString() ;
+                                Connection = $conn_obj; 
+                                Database = $DataBase
+                                }
+
+            [void]$Global:sqliteconn.Add($NewConnection)
+            # Return the connection object.
+            $NewConnection
+        }
+        else
+        {
+            Write-Warning "A connection to $DataBase already exists."
+        }
+    }
+
+    End
+    {
+    }
+
+}
+
+
+function Remove-SQLite3Connection
+{
+    [CmdletBinding()]
+    param( 
+        [Parameter(Mandatory=$false)]
+        [Int32[]] $Index
+    )
+    Begin{
+        $currentConnections = @()
+        foreach($conn in $Global:sqliteconn) {$currentConnections += $conn}
+    }
+    Process
+    {
+        if ($Index.Length -gt 0)
+        {
+            foreach($i in $Index)
+            {
+                foreach($Connection in $currentConnections)
+                {
+                    if ($Connection.Index -eq $i)
+                    {
+                        Write-Verbose "Removing connection with Index $i"
+                        $Connection.connection.close()
+                        $Global:sqliteconn.Remove($Connection)
+                        Write-Verbose "Connection removed."
+                    }
+                }
+            }
+        }
+        else
+        {
+            # Return all database connections.
+            $return_sessions = @()
+            foreach($s in $Global:sqliteconn){$return_sessions += $s}
+            $return_sessions
+        }
+    }
+    End{}
+
+}
+
+
+function Get-SQLite3Connection 
+{
+    [CmdletBinding()]
+    param( 
+        [Parameter(Mandatory=$false)]
+        [Int32[]] $Index
+    )
+
+    Begin{}
+    Process
+    {
+        if ($Index.Length > 0)
+        {
+            foreach($i in $Index)
+            {
+                foreach($Connection in $Global:sqliteconn)
+                {
+                    if ($Connection.Index -eq $i)
+                    {
+                        $Connection
+                    }
+                }
+            }
+        }
+        else
+        {
+            # Return all database connections.
+            $return_sessions = @()
+            foreach($s in $Global:sqliteconn){$return_sessions += $s}
+            $return_sessions
+        }
+    }
+    End{}
+}
+
+
+function Invoke-SQLite3Query           
+{
+    [CmdletBinding()]            
+    param( 
+        [Parameter(Mandatory=$true)]
+        [string]$SQL,
+
+        [Parameter(Mandatory=$true,
+        ParameterSetName = "Index")]
+        [int32[]]$Index,
+
+        [Parameter(Mandatory=$true,
+        ParameterSetName = "Connection")]
+        [PSobject]$Connection            
+        )
+    if ($Index.Length -gt 0)
+    {
+        foreach($conn in $Global:sqliteconn)
+        {
+            if ($conn.index -in $Index)
+            {
+                $cmd = new-object System.Data.SQLite.SQLiteCommand($SQL,$conn.Connection)            
+                $ds = New-Object system.Data.DataSet            
+                $da = New-Object System.Data.SQLite.SQLiteDataAdapter($cmd)            
+                $da.fill($ds) | Out-Null            
+                return $ds.tables[0]
+            }
+        } 
+    }
+    elsif ($Connection -ne $null)
+    {
+        $cmd = new-object System.Data.SQLite.SQLiteCommand($SQL,$Connection.Connection)            
+        $ds = New-Object system.Data.DataSet            
+        $da = New-Object System.Data.SQLite.SQLiteDataAdapter($cmd)            
+        $da.fill($ds) | Out-Null            
+        return $ds.tables[0]
+    }         
+}
---
+++ Utility/utility.psm1
@@ -0,0 +1,221 @@
+﻿ Function Get-WebFile
+ {
+     Param(
+        [Parameter(Mandatory = $true)]
+        [uri]$URL,
+
+        [Parameter(Mandatory = $true)]
+        $LocalFile
+     )
+  
+    Begin
+    {
+        $WebClient = New-Object System.Net.WebClient
+    }
+    
+    Process
+    {
+        try 
+        {
+            Register-ObjectEvent $WebClient DownloadProgressChanged -action {     
+
+                Write-Progress -Activity "Downloading" -Status `
+                    ("{0} of {1}" -f $eventargs.BytesReceived, $eventargs.TotalBytesToReceive) `
+                    -PercentComplete $eventargs.ProgressPercentage    
+            }
+
+            Register-ObjectEvent $client DownloadFileCompleted -SourceIdentifier Finished
+            $WebClient.DownloadFileAsync($URL, $LocalFile)
+
+            # optionally wait, but you can break out and it will still write progress
+            Wait-Event -SourceIdentifier Finished
+
+        } 
+        finally 
+        { 
+            $WebClient.dispose()
+        }
+    }
+    End{}
+}
+
+
+function New-Zip
+{
+	[CmdletBinding()]
+    Param
+    (
+	[Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+	$ZipFile
+	)
+	set-content $ZipFile ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+	(dir $ZipFile).IsReadOnly = $false
+}
+
+
+function Add-Zip
+{
+	[CmdletBinding()]
+    Param
+    (
+	[Parameter(Mandatory=$true)]
+		$ZipFile,
+		
+		[Parameter(Mandatory=$true,
+                  ValueFromPipeline=$true,
+                   Position=1)]
+        [ValidateScript({Test-Path $_})]
+		$File
+		
+	)
+	if(-not (test-path($ZipFile)))
+	{
+		set-content $ZipFile ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+		(dir $ZipFile).IsReadOnly = $false	
+	}
+	Write-Verbose $File
+	$shellApplication = new-object -com shell.application
+	$zipPackage = (new-object -com shell.application).NameSpace(((get-item $ZipFile).fullname))
+	$zipPackage.CopyHere((get-item $File).FullName)
+}
+
+
+function Get-ZipChildItems_Recurse 
+{
+    [CmdletBinding()]
+	param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [object]$items
+    )
+     
+    foreach($si in $items) 
+    { 
+        if($si.getfolder -ne $null) 
+        { 
+            Get-ZipChildItems_Recurse $si.getfolder.items() 
+        } 
+      $si | select path
+      } 
+}
+
+
+function Get-Zip
+{
+	[CmdletBinding()]
+    Param
+    (
+	    [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [ValidateScript({Test-Path $_})]
+		$ZipFile,
+		$Recurse = $true
+	)
+
+	$shellApplication = new-object -com shell.application
+	$zipPackage = $shellApplication.NameSpace(((get-item $ZipFile).fullname))
+	if ($Recurse -eq $false)
+	{
+		$zipPackage.Items() | select path
+	}
+	else
+	{
+		Get-ZipChildItems_Recurse $zipPackage.Items()
+	}
+}
+
+
+function Expand-Zip
+{
+	[CmdletBinding()]
+    Param
+    (
+	    [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [ValidateScript({Test-Path $_})]
+		$ZipFile,
+		
+	    [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+		$Destination
+	)
+	
+	if (Test-Path -PathType Container $Destination)
+	{
+		$destinationFolder = $shellApplication.NameSpace(((get-item $Destination).fullname))
+	}
+	else
+	{
+		New-Item -ItemType container -Path $Destination | Out-Null
+		$destinationFolder = $shellApplication.NameSpace(((get-item $Destination).fullname))
+	}
+
+	$shellApplication = new-object -com shell.application
+	$zipPackage = $shellApplication.NameSpace(((get-item $ZipFile).fullname))
+	$destinationFolder = $shellApplication.NameSpace(((get-item $Destination).fullname))
+	$destinationFolder.CopyHere($zipPackage.Items())
+}
+
+function Get-FileHash 
+{
+	<#
+		.SYNOPSIS
+			cmdlet for calculatingt the hash of a given file.
+
+		.DESCRIPTION
+			Calculates either the MD5, SHA1, SHA256, SHA384 or SHA512 checksum of a given file.
+
+		.PARAMETER  File
+			The description of the ParameterA parameter.
+
+		.PARAMETER  HashAlgorithm
+			The description of the ParameterB parameter.
+
+		.EXAMPLE
+			PS C:\> Get-Something -ParameterA 'One value' -ParameterB 32
+	#>
+	[CmdletBinding()]
+	[OutputType([string])]
+	param(
+		[Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [ValidateScript({Test-Path $_})] 
+        $File,
+
+		[ValidateSet("MD5", "SHA1", "SHA256", "SHA384", "SHA512")]
+    	$HashAlgorithm = "MD5"
+	)
+	 Begin
+    {
+		$hashType = [Type] "System.Security.Cryptography.$HashAlgorithm"
+		$hasher = $hashType::Create()
+	}
+	
+	Process
+	{
+		$inputStream = New-Object IO.StreamReader $File
+    	$hashBytes = $hasher.ComputeHash($inputStream.BaseStream)
+    	$inputStream.Close()
+
+   		 # Convert the result to hexadecimal
+    	$builder = New-Object System.Text.StringBuilder
+    	Foreach-Object -Process { [void] $builder.Append($_.ToString("X2")) } -InputObject $hashBytes
+		# Create Object
+    	$output = New-Object PsObject -Property @{
+        		Path = ([IO.Path]::GetFileName($file));
+        		HashAlgorithm = $hashAlgorithm;
+        		HashValue = $builder.ToString()
+			}
+	}
+	End
+	{
+		$output
+	}
+}
