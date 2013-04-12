e1a6f1c8d47e21af42959058d1c8ac7d2ef3300e
Carlos Perez <carlos_perez@darkoperator.com>
2013-04-12 12:44:20 -0400

    added help comments and updated the functions for SQLite3 Commands

--- Database/Database.psm1
+++ Database/Database.psm1
@@ -1,37 +1,63 @@
 ﻿$Global:sqliteconn = New-Object System.Collections.ArrayList
 
+
+
+<#
+.Synopsis
+   Creates a connection to a SQLite3 Database
+.DESCRIPTION
+   Creates a connection to a SQLite3 Database file and stores the connection in to $Global:sqliteconn.
+.EXAMPLE
+   Opens database main.db and creates a connection object for it.
+
+   PS C:\> Connect-SQLite3 -DataBase .\main.db
+
+    Connection                             Database                               Index                                 
+    ----------                             --------                               -----                                 
+   System.Data.SQLite.SQLiteConnection    .\main.db                              0  
+#>
 function Connect-SQLite3 
 {
     [CmdletBinding()]
 	param (
+
+        # Databse file to open.
         [Parameter(Mandatory=$true,
                    ValueFromPipelineByPropertyName=$true,
                    Position=0)]
         [ValidateScript({Test-Path $_})]
-        [string]$DataBase,
-        [switch]$ReadOnly,
-        [SecureString]$Password
+        [string]$DataBase
+
+        # Open Database ReadOnly also.
+        #[switch]$ReadOnly,
+
+        # Password for opening the database if it requieres it.
+        #[SecureString]$Password
     )
     
     Begin
     {
+        $x86Assembly = "$($PSScriptRoot)\x86\System.Data.SQLite.dll"
+        $x64Assembly = "$($PSScriptRoot)\x64\System.Data.SQLite.dll"
+
         # Load the appropiate DLL Depending on the Archiecture
 	    switch ([intptr]::size)
 	    {
-		    4 {$sqlitedll = [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\x86\System.Data.SQLite.dll")} 
-		    8 {$sqlitedll = [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\x64\System.Data.SQLite.dll")}
+		    4 {$sqlitedll = [System.Reflection.Assembly]::LoadFrom($x86Assembly)} 
+		    8 {$sqlitedll = [System.Reflection.Assembly]::LoadFrom($x64Assembly)}
 	    }
+        
+        $DataBaseFile = (Get-ItemProperty $DataBase).FullName
     }
     Process
     {
 	    $cn = New-Object -TypeName System.Data.SQLite.SQLiteConnection
-	    $cn.ConnectionString = "Data Source=$DataBase"
+	    $cn.ConnectionString = "Data Source=$DataBaseFile"
 	    $cn.Open()
         $conn_obj = $cn
         if ($Global:sqliteconn -notcontains $conn_obj)
         {
             $SessionIndex = $Global:sqliteconn.Count
-            write "index is $SessionIndex"
             $NewConnection = New-Object psobject -Property @{
                                 Index = $SessionIndex.ToString() ;
                                 Connection = $conn_obj; 
@@ -55,12 +81,41 @@ function Connect-SQLite3
 }
 
 
+
+
+<#
+.Synopsis
+   Removes a specific SQLite3 connection
+.DESCRIPTION
+   Removes a specific SQLite3 connection given its Index
+.EXAMPLE
+   Disconnect all SQLite3 connections
+
+   PS C:\> Get-SQLite3Connection | Remove-SQLite3Connection
+
+   Connection                             Database                               Index                                 
+   ----------                             --------                               -----                                 
+   System.Data.SQLite.SQLiteConnection    .\main.db                              0                                     
+
+.EXAMPLE
+   Remove a SQLite3 connection given its index
+
+   PS C:\> Remove-SQLite3Connection -Index 0
+
+   Connection                             Database                               Index                                 
+   ----------                             --------                               -----                                 
+   System.Data.SQLite.SQLiteConnection    .\main.db                              0                                     
+
+#>
 function Remove-SQLite3Connection
 {
     [CmdletBinding()]
     param( 
-        [Parameter(Mandatory=$false)]
-        [Int32[]] $Index
+        # Index for the database connection.
+        [Parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true)]
+        [Int32] $Index
+        
     )
     Begin{
         $currentConnections = @()
@@ -68,15 +123,16 @@ function Remove-SQLite3Connection
     }
     Process
     {
-        if ($Index.Length -gt 0)
+        if ($Index -ge 0)
         {
+            Write-Verbose "Removing connection with Index $Index"
             foreach($i in $Index)
             {
                 foreach($Connection in $currentConnections)
                 {
                     if ($Connection.Index -eq $i)
                     {
-                        Write-Verbose "Removing connection with Index $i"
+                        Write-Verbose "Connection Found"
                         $Connection.connection.close()
                         $Global:sqliteconn.Remove($Connection)
                         Write-Verbose "Connection removed."
@@ -84,31 +140,40 @@ function Remove-SQLite3Connection
                 }
             }
         }
-        else
-        {
-            # Return all database connections.
-            $return_sessions = @()
-            foreach($s in $Global:sqliteconn){$return_sessions += $s}
-            $return_sessions
-        }
     }
     End{}
 
 }
 
 
+
+<#
+.Synopsis
+   Get SQLite3 Connections
+.DESCRIPTION
+   Get all or a specified existing SQLite3 Connection.
+.EXAMPLE
+   Gets all SQLIte3 Connections 
+   
+   PS C:\> Get-SQLite3Connection
+
+    Connection                             Database                               Index                                 
+    ----------                             --------                               -----                                 
+    System.Data.SQLite.SQLiteConnection    .\main.db                              0     
+
+#>
 function Get-SQLite3Connection 
 {
     [CmdletBinding()]
     param( 
         [Parameter(Mandatory=$false)]
-        [Int32[]] $Index
+        [Int32] $Index
     )
 
     Begin{}
     Process
     {
-        if ($Index.Length > 0)
+        if ($Index)
         {
             foreach($i in $Index)
             {
@@ -133,6 +198,19 @@ function Get-SQLite3Connection
 }
 
 
+
+<#
+.Synopsis
+   Exsecutes SQL query against SQLite3 Connection
+.DESCRIPTION
+   Exsecutes SQL query against SQLite3 Connection against an existing SQLite3 Connection
+.EXAMPLE
+   Execute query to list all the tables in the database.
+
+   PS C:\> Invoke-SQLite3Query -SQL "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;" -Index 0
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
 function Invoke-SQLite3Query           
 {
     [CmdletBinding()]            
@@ -142,14 +220,16 @@ function Invoke-SQLite3Query
 
         [Parameter(Mandatory=$true,
         ParameterSetName = "Index")]
-        [int32[]]$Index,
+        [int32]$Index,
 
         [Parameter(Mandatory=$true,
         ParameterSetName = "Connection")]
         [PSobject]$Connection            
         )
-    if ($Index.Length -gt 0)
+    if ($Index -ge 0)
     {
+        Write-Verbose "Executing Query $SQL"
+        Write-Verbose "Executing against $Index"
         foreach($conn in $Global:sqliteconn)
         {
             if ($conn.index -in $Index)
@@ -157,7 +237,7 @@ function Invoke-SQLite3Query
                 $cmd = new-object System.Data.SQLite.SQLiteCommand($SQL,$conn.Connection)            
                 $ds = New-Object system.Data.DataSet            
                 $da = New-Object System.Data.SQLite.SQLiteDataAdapter($cmd)            
-                $da.fill($ds) | Out-Null            
+                $da.fill($ds) | Out-Null       
                 return $ds.tables[0]
             }
         } 
